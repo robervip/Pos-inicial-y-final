59b99981a868a688916eb88ae5cd823f08d04edd
robervip <44229332+robervip@users.noreply.github.com>
2018-11-08 10:54:11 +0100

    Update codigo1.js

--- codigo1.js
+++ codigo1.js
@@ -5,21 +5,25 @@ var filaInicial=0;
 var columnaFinal=0;
 var filaFinal=0;
 function casillaAleatoria(){
-  columnaInicial=Math.floor(Math.random() * 6 + 1);
-  posInicial[0]=columnaInicial;
-  filaInicial=Math.floor(Math.random() * 6 + 1);
-  posInicial[1]=filaInicial;
-  columnaFinal=Math.floor(Math.random() * 6 + 1);
-  posFinal[0]=columnaFinal;
-  filaFinal=Math.floor(Math.random() * 6 + 1);
-  posFinal[1]=filaFinal;
-  if (posInicial[0,1]=posFinal[0,1]) {
+  while (true) {
+    columnaInicial=Math.floor(Math.random() * 6 + 1);
     columnaFinal=Math.floor(Math.random() * 6 + 1);
-    posFinal[0]=columnaFinal;
-    filaFinal=Math.floor(Math.random() * 6 + 1);
-    posFinal[1]=filaFinal;
-}
+    if (columnaInicial!=columnaFinal) {
+      break;
   }
+}
+  while (true) {
+    filaInicial=Math.floor(Math.random() * 6 + 1);
+    filaFinal=Math.floor(Math.random() * 6 + 1);
+    if (filaInicial!=filaFinal) {
+      break;
+    }
+  }
+}
+posInicial.splice(`0,1,${columnaInicial}`);
+posFinal.splice(`0,1,${columnaFinal}`);
+posInicial.splice(`1,1,${filaInicial}`);
+posFinal.splice(`1,1,${filaFinal}`);
 casillaAleatoria();
 console.log(`La posicion inicial es: Columna ${posInicial[0]} Fila ${posInicial[1]}`);
 console.log(`La posicion final es: Columna ${posFinal[0]} Fila ${posFinal[1]}`);
